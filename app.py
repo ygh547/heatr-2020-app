59a9dc3a981b2d9bf5d80718544df3c3fb1b2b09
ygh547 <ygh547@naver.com>
2022-05-26 15:11:03 +0900

    add

---
+++ app.py
@@ -0,0 +1,27 @@
+from cProfile import run
+import streamlit as st
+from tkinter import Menu
+from app_eda import run_eda
+
+from app_home import run_home
+from app_ml import run_ml
+
+
+
+def main():
+
+    st.title('건강상태 예측해서 사전에 알려주기')
+
+    menu = ['Home', 'EDA', 'ML']
+    choice = st.sidebar.selectbox('메뉴 선택', menu)
+
+    if choice == menu[0] :
+        run_home()
+    elif choice == menu[1] :
+        run_eda()
+    elif choice == menu[2] :
+        run_ml()
+
+
+if __name__ == '__main__' :
+    main()
